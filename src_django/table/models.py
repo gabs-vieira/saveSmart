3f979072058e96ecf9ec4570cae01261aa1d84ad
Your Name <you@example.com>
2024-11-01 17:22:25 -0300

    Alterando o nome da pasta src_django

--- src_django/table/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Table(models.Moldel):
-    id_table = models.IntegerField(primary_key=True,null=False,blank=False)
-
-#creating the value of the table in sql
--- src_django/user/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    name = models.CharField(blank=False,null=False)
-    email = models.EmailField(blank=False,null=False)
-    id_user = models.IntegerField(blank=False,null=False)
-
-#creating the value of the table in sql
