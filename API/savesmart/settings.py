901e0c269c358fc147b95b4d867c84a79a81490f
Your Name <you@example.com>
2024-11-01 19:20:05 -0300

    Adding

--- API/savesmart/settings.py
+++ API/savesmart/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config , Csv
+from decouple import config, Csv
+from dj_database_url import parse as db_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,10 +80,7 @@ WSGI_APPLICATION = 'savesmart.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': db_url(config('DATABASE_URL'))
 }
 
 
