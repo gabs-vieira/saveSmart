13d2c6333496631701f71b9c9c4fdb68212ce6d6
Gabriel Vieira <gabriel@testbox.com>
2024-11-24 17:19:59 -0300

    feat: user crud

--- api/api/urls.py
+++ api/api/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.db import router
+from django.urls import include, path
+from users.views import UserViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r"users", UserViewSet, basename="user")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
